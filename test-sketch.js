eb558de1fd918d00523173dad26ab4d2e78cf154
stackenziemewart <156844313+stackenziemewart@users.noreply.github.com>
2024-01-30 12:26:25 -0800

    Add files via upload

--- test-sketch.js
+++ test-sketch.js
@@ -1,35 +1,120 @@
-function setup() {
-  createCanvas(710, 400, WEBGL);
+// Drawn from https://p5js.org/examples/simulate-penrose-tiles.html
+// This is a port by David Blitz of the "Penrose Tile" example from processing.org/examples
 
-  describe(
-    'a 3d example containing a spinning box and a sphere, each lit with a number of different lights, including ambient (gray), directional (red), spotlight (green), and point (blue).'
-  );
+var ds;
+
+function setup() {
+  createCanvas(710, 400);
+  ds = new PenroseLSystem();
+  //please, play around with the following line
+  ds.simulate(5);
 }
 
 function draw() {
   background(0);
-
-  let locX = mouseX - height / 2;
-  let locY = mouseY - width / 2;
-
-  // ambient light is gray
-  ambientLight(50);
-  // directional light is red
-  directionalLight(255, 0, 0, 0.25, 0.25, 0);
-  // spotlight is green
-  spotLight(0, 255, 0, 150, 0, 250, 0, 0, -1);
-  // point light is blue
-  pointLight(0, 0, 255, locX, locY, 250);
-
-  push();
-  translate(-width / 4, 0, 0);
-  rotateZ(frameCount * 0.02);
-  rotateX(frameCount * 0.02);
-  specularMaterial(250);
-  box(100, 100, 100);
-  pop();
-
-  translate(width / 4, 0, 0);
-  ambientMaterial(250);
-  sphere(120, 24);
+  ds.render();
 }
+
+function PenroseLSystem() {
+    this.steps = 0;
+
+   //these are axiom and rules for the penrose rhombus l-system
+   //a reference would be cool, but I couldn't find a good one
+    this.axiom = "[X]++[X]++[X]++[X]++[X]";
+    this.ruleW = "YF++ZF----XF[-YF----WF]++";
+    this.ruleX = "+YF--ZF[---WF--XF]+";
+    this.ruleY = "-WF++XF[+++YF++ZF]-";
+    this.ruleZ = "--YF++++WF[+ZF++++XF]--XF";
+
+    //please play around with the following two lines
+    this.startLength = 460.0;
+    this.theta = TWO_PI / 10.0; //36 degrees, try TWO_PI / 6.0, ...
+    this.reset();
+}
+
+PenroseLSystem.prototype.simulate = function (gen) {
+  while (this.getAge() < gen) {
+    this.iterate(this.production);
+  }
+}
+
+PenroseLSystem.prototype.reset = function () {
+    this.production = this.axiom;
+    this.drawLength = this.startLength;
+    this.generations = 0;
+  }
+
+PenroseLSystem.prototype.getAge = function () {
+    return this.generations;
+  }
+
+//apply substitution rules to create new iteration of production string
+PenroseLSystem.prototype.iterate = function() {
+    var newProduction = "";
+
+    for(var i=0; i<this.production.length; ++i) {
+      var step = this.production.charAt(i);
+      //if current character is 'W', replace current character
+      //by corresponding rule
+      if (step == 'W') {
+        newProduction = newProduction + this.ruleW;
+      }
+      else if (step == 'X') {
+        newProduction = newProduction + this.ruleX;
+      }
+      else if (step == 'Y') {
+        newProduction = newProduction + this.ruleY;
+      }
+      else if (step == 'Z') {
+        newProduction = newProduction + this.ruleZ;
+      }
+      else {
+        //drop all 'F' characters, don't touch other
+        //characters (i.e. '+', '-', '[', ']'
+        if (step != 'F') {
+          newProduction = newProduction + step;
+        }
+      }
+    }
+
+    this.drawLength = this.drawLength * 0.5;
+    this.generations++;
+    this.production = newProduction;
+}
+
+//convert production string to a turtle graphic
+PenroseLSystem.prototype.render = function () {
+    translate(width/2, height/2);
+
+    this.steps += 20;
+    if(this.steps > this.production.length) {
+      this.steps = this.production.length;
+    }
+
+    for(var i=0; i<this.steps; ++i) {
+      var step = this.production.charAt(i);
+
+      //'W', 'X', 'Y', 'Z' symbols don't actually correspond to a turtle action
+      if( step == 'F') {
+        stroke(255, 60);
+        for(var j=0; j < this.repeats; j++) {
+          line(0, 0, 0, -this.drawLength);
+          noFill();
+          translate(0, -this.drawLength);
+        }
+        this.repeats = 1;
+      }
+      else if (step == '+') {
+        rotate(this.theta);
+      }
+      else if (step == '-') {
+        rotate(-this.theta);
+      }
+      else if (step == '[') {
+        push();
+      }
+      else if (step == ']') {
+        pop();
+      }
+    }
+  }
